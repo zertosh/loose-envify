e3ab69821fe019fff51999ca8278ee8200298019
Andres Suarez <zertosh@gmail.com>
2015-09-20 16:39:37 -0700

    add default env test

--- test/default.js
+++ test/default.js
@@ -1,7 +1,23 @@
 'use strict';
 
+var browserify = require('browserify');
 var test = require('tap').test;
 
 test('default', function(t) {
-  t.end();
+  t.plan(3);
+
+  process.env.NODE_ENV = 'production';
+  var LooseEnvify = require('../default');
+  process.env.NODE_ENV = 'development';
+
+  var b = browserify({
+    entries: [__dirname + '/react/react-with-addons-with-node_env.js']
+  });
+  b.transform(LooseEnvify);
+
+  b.bundle(function(err, src) {
+    t.notOk(err);
+    t.match(String(src), /"production"/);
+    t.notMatch(String(src), /"development"/);
+  });
 });
